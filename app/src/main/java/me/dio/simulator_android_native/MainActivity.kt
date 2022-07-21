2287b1a2bef3b1739829aec8506274c311176c4e
Loucong <loucongxd@gmail.com>
2022-07-21 14:37:49 -0300

    first commit

---
+++ app/src/main/java/me/dio/simulator_android_native/MainActivity.kt
@@ -0,0 +1,11 @@
+package me.dio.simulator_android_native
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
