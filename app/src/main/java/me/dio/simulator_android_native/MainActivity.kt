c7e1cbe3fd38723da3a641831f9fe454f7097423
Loucong <loucongxd@gmail.com>
2022-07-22 15:13:32 -0300

    ic_launcher added Start build app Swipe Refresh Layout Added

--- app/src/main/java/me/dio/simulator_android_native/MainActivity.kt
+++ app/src/main/java/me/dio/simulator_android_native/MainActivity.kt
@@ -1,11 +1,19 @@
 package me.dio.simulator_android_native
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.dio.simulator_android_native.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
