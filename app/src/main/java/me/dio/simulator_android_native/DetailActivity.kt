8faac038653766a7e5e934e78ffb5b93d31dd3e4
Loucong <loucongxd@gmail.com>
2022-07-23 13:11:24 -0300

    Layouts created

---
+++ app/src/main/java/me/dio/simulator_android_native/DetailActivity.kt
@@ -0,0 +1,20 @@
+package me.dio.simulator_android_native
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.dio.simulator_android_native.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
