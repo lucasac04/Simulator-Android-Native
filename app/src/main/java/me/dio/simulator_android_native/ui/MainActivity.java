259485c6c5bc36e6300603f46061db78307572ff
Loucong <loucongxd@gmail.com>
2022-07-27 15:43:01 -0300

    FireBase Added

--- app/src/main/java/me/dio/simulator_android_native/ui/MainActivity.java
+++ app/src/main/java/me/dio/simulator_android_native/ui/MainActivity.java
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -28,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     private MatchesApi matchesApi;
-    private MatchesAdapter matchesAdapter;
+    private MatchesAdapter matchesAdapter = new MatchesAdapter(Collections.emptyList());
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -41,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
         setupMatchesList();
         setupMatchesRefresh();
         setupFloatingActionButton();
+
     }
 
     private void setupHttpClient() {
@@ -55,7 +57,9 @@ public class MainActivity extends AppCompatActivity {
     private void setupMatchesList() {
         binding.rvMatches.setHasFixedSize(true);
         binding.rvMatches.setLayoutManager(new LinearLayoutManager(this));
+        binding.rvMatches.setAdapter(matchesAdapter);
         findMatchesFromApi();
+
     }
 
     private void setupMatchesRefresh() {
@@ -63,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupFloatingActionButton() {
+
         binding.fabSimulate.setOnClickListener(view -> {
             view.animate().rotationBy(360).setDuration(500).setListener(new AnimatorListenerAdapter() {
                 Random random = new Random();
@@ -106,4 +111,5 @@ public class MainActivity extends AppCompatActivity {
     private void showErrorMessage() {
         Snackbar.make(binding.fabSimulate, R.string.error_api, Snackbar.LENGTH_LONG).show();
     }
+
 }
